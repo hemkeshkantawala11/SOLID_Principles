26b0fe6af843e9f03702387b1486ae8be1d092b4
hemkeshkantawala11 <hemkesh123kantawala@gmail.com>
2025-08-31 21:45:44 +0530

    deleted solid Pleaseenter the commit message for your changes. Lines starting

--- SST27-LLD101/SOLID/ex05/src/Rectangle.java
+++ SST27-LLD101/SOLID/ex05/src/Rectangle.java
@@ -4,7 +4,16 @@ public class Rectangle implements Shape {
         this.w = w;
         this.h = h;
     }
+    @Override
     public int area(){ 
         return w*h; 
+    }
+
+    public void setWidth(int w){ 
+        this.w = w; 
+    }
+
+    public void setHeight(int h){ 
+        this.h = h; 
     }
 }
