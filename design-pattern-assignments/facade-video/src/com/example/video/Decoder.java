8979f2260f306f34555f5b2f1c01a42b6fe7baa7
hemkeshkantawala11 <hemkesh123kantawala@gmail.com>
2025-09-07 17:06:05 +0530

    feat: design pattern facade video assignment completed

--- design-pattern-assignments/facade-video/src/com/example/video/Decoder.java
+++ design-pattern-assignments/facade-video/src/com/example/video/Decoder.java
@@ -3,6 +3,8 @@ import java.nio.file.Path;
 
 public class Decoder {
     public Frame[] decode(Path src) { 
-        return new Frame[] { new Frame(1920,1080), new Frame(1920,1080) }; 
+        return new Frame[] { 
+            new Frame(1920,1080), new Frame(1920,1080) 
+        }; 
     }
 }
