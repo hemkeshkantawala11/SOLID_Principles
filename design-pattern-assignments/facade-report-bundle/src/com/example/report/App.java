d87780d5603054d0e054a09acab3c21770d30230
hemkeshkantawala11 <hemkesh123kantawala@gmail.com>
2025-09-07 12:41:21 +0530

    feat: design pattern facade report bundle assignment completed

--- design-pattern-assignments/facade-report-bundle/src/com/example/report/App.java
+++ design-pattern-assignments/facade-report-bundle/src/com/example/report/App.java
@@ -1,16 +1,16 @@
 package com.example.report;
-import java.nio.file.*; import java.util.*;
+import java.nio.file.Path;
+ import java.util.Map;
 
 public class App {
     public static void main(String[] args) {
         Map<String,Object> data = Map.of("name", "Quarterly");
-        JsonWriter jw = new JsonWriter(); 
-        Zipper z = new Zipper(); 
-        AuditLog log = new AuditLog();
-        Path json = jw.write(data, Path.of("out"), "report");
-        Path zip = z.zip(json, Path.of("out", "report.zip"));
-        log.log("exported " + zip);
-        System.out.println("DONE " + zip);
+        ReportBundleFacade facade = new ReportBundleFacade(new AuditLog(), new JsonWriter(), new Zipper());
+        // JsonWriter jw = new JsonWriter(); 
+        // Zipper z = new Zipper(); 
+        // AuditLog log = new AuditLog();
+        Path json = facade.export(data, Path.of("out"), "report");
+        System.out.println("DONE " + json);
         // TODO: Replace the above with a single call to ReportBundleFacade.export(...)
     }
 }
